836741bdc9b935442b97c73d583f077418ebb807
Christian Royal <croyal04@gmail.com>
2019-07-21 22:16:24 -0400

    Dexter Running Program Runs Doesn't populate anything yet.

--- app/src/main/java/com/example/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/weatherapi/MainActivity.java
@@ -1,37 +1,137 @@
 package com.example.weatherapi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
+
+import android.util.Log;
 import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.weatherapi.Adapter.ViewPagerAdapter;
+
+import com.example.weatherapi.common.Common;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
     private CoordinatorLayout coordinatorLayout;
-    private FusedLocationProviderClient fusedLocationProviderClient;
+    private FusedLocationProviderClient mFusedLocationClient;
     private LocationCallback locationCallback;
     private LocationRequest locationRequest;
-
-
-
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Success");
+
+
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.root_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        //Request Permission w Dexter
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        Log.d(TAG, "onPermissionsChecked: Permissions Success");
+                        if (report.areAllPermissionsGranted()) {
+
+                            buildLocationRequest();
+                            buildLocationCallBack();
+
+
+                            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                                return;
+                            }
+
+                            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+                            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+                        }
+                    }
+
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        Snackbar.make(coordinatorLayout, "Permission Denied", Snackbar.LENGTH_LONG).show();
+                        Log.d(TAG, "onPermissionRationaleShouldBeShown: Failure");
+                    }
+                }).check();
+    }
+
+    private void buildLocationCallBack() {
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Common.current_location = locationResult.getLastLocation();
+
+                viewPager = (ViewPager)findViewById(R.id.view_pager);
+                setupViewPager(viewPager);
+                tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+                tabLayout.setupWithViewPager(viewPager);
+
+                Log.d(TAG, "locationResult.getLastLocation().getLatitude()+\"/\"+locationResult.getLastLocation().getLongitude()");
+
+            }
+        };
+
+    }
+
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10.0f);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(CurrentWeather.getInstance(), "Today");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+
+
     }
 }
--- app/src/main/java/com/example/weatherapi/common/Common.java
+++ app/src/main/java/com/example/weatherapi/common/Common.java
@@ -1,5 +1,8 @@
 package com.example.weatherapi.common;
 
+import android.location.Location;
+
 public class Common {
     public static final String APP_ID="9732ccce9f6658f4d8087467579981fe";
+    public static Location current_location=null;
 }
